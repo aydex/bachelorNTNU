3d1305eee7288187b893d6c989cbcdbb54454d1b
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-02-15 06:19:22 -0500

    Testing commits

---
+++ api/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrianh
+ * Date: 09.02.16
+ * Time: 10.32
+ */
+
+$config = parse_ini_file("../bachelor.ini");
+echo $config;
+
+$db_name = $config[0];
+$hostname = $config[1];
+$username = $config[2];
+$password = $config[3];
---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+    <title>Bachelor 9</title>
+    <meta charset="UTF-8">
+	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>	
+  </head>
+  <body>
+    Bachelor gruppe 9
+    <?php echo date("Y"); ?>
+  </body>
+</html>
