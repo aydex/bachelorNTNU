4ff346e5231de8795260bfc3d047d2113f0d3688
Aydex <adrian.hundseth@hotmail.com>
2016-02-15 13:02:36 +0100

    added some config for the php -> mysql connection
    this requires the bachelor.ini file in root to function, which contains,
    among other things, the password to the database.
    this has been purposely left out for security reasons.

--- api/config.php
+++ api/config.php
@@ -5,3 +5,14 @@
  * Date: 09.02.16
  * Time: 10.32
  */
+
+// set up the connection variables
+$config = parse_ini_file("../bachelor.ini");
+
+$db_name = $config[db_name];
+$hostname = $config[hostname];
+$username = $config[username];
+$password = $config[password];
+
+// connect to the database
+$dbh = new PDO("mysql:host=$hostname;dbname=$db_name", $username, $password);
