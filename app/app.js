0989815c1816c4f22da66d9bd12a35d5e51d1be7
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-02-18 14:40:07 +0100

    moved kasperrt test to app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,32 @@
 /**
  * Created by adrianh on 09.02.16.
  */
+
+angular.module('myApp', []).controller('namesCtrl', function($scope, $http, $timeout) {
+    var _timeout;
+
+    $scope.reverse  = false;
+
+    $scope.searchDelay = function(){
+        if(_timeout){ //if there is already a timeout in process cancel it
+            $timeout.cancel(_timeout);
+        }
+        _timeout = $timeout(function(){
+            console.log("loading things");
+            $http.post("../api/test.php?name=" + $scope.searchName)
+                .then(function (response) {$scope.names = response.data.records;});
+
+            _timeout = null;
+        },500);
+    }
+
+    $scope.orderByMe = function(x) {
+        if($scope.myOrderBy != x){
+            $scope.reverse = !$scope.reverse;
+        }
+        $scope.myOrderBy = x;
+    }
+    $scope.reverseOrder = function(){
+        $scope.reverse = !$scope.reverse;
+    }
+});
