e68c67adeff6c3aad7717375d69f4d472e65a420
Aydex <adrian.hundseth@hotmail.com>
2016-03-07 10:09:29 +0100

    removed folder js

--- js/filters.js
+++
@@ -1,7 +0,0 @@
-angular.module('SvgMapApp').filter('map_colour', [function () {
-    return function (input) {
-        var b = 255 - Math.floor(input * 255);
-        var g = Math.floor(input * 255);
-        return "rgba(255," + g + "," + b + ",1)";
-    }
-}]);
