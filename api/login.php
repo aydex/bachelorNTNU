284bd0d558ac99685a603103ba11830e8b7d507a
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-04-15 12:09:42 +0200

    Fixed error where trying to log into an already logged in system crashes

--- api/login.php
+++ api/login.php
@@ -12,8 +12,11 @@ $provider = new \ChrisHemmings\OAuth2\Client\Provider\Drupal([
 
 if (isset($_GET['state']) && isset($_GET['code']) && isset($_SESSION['oauth2state'])) {
     if (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+        unset($_COOKIE['name']);
+        setcookie('name', null, -1, '/');
+        unset($_SESSION['loggedIn']);
         unset($_SESSION['oauth2state']);
-        echo json_encode(array("records" => "login_required"));
+        header('Location: /');
     } else {
         // Try to get an access token (using the authorization code grant)
         $token = $provider->getAccessToken('authorization_code', [
@@ -54,11 +57,7 @@ if (isset($_GET['state']) && isset($_GET['code']) && isset($_SESSION['oauth2stat
             	$_SESSION["loggedIn"] = true;
             	setcookie("name", $body2["realname"], 0, "/"); 
             	header('Location: /search');
-                //print_r("logged in");
             }
-           	// Sample result $body:
-            // { "uid": "5330", "name": "testuser_5330", "mail": "testuser@ramsalt.com", "theme": "", "signature": "", "signature_format": "full_html", "created": "1460358464", "access": "1460460439", "login": "1460459194", "status": "1", "timezone": "Europe\/Oslo", "language": "en", "picture": "0", "init": "testuser@ramsalt.com", "data": false, "roles": { "2": "authenticated user" }, "field_first_name": { "und": [ { "value": "George", "format": null, "safe_value": "George" } ] }, "field_last_name": { "und": [ { "value": "Ramsalt", "format": null, "safe_value": "Ramsalt" } ] }, "field_user_type": { "und": [ { "value": "annet" } ] }, "domain_user": { "1": "1", "5": "5" }, "realname": "George Ramsalt" }
-
         } catch (Exception $e) {
             exit('ERROR: ' . $e->getMessage());
         }
