103d7717addfaea4cf57916b6e25aabe399859b4
lqasse <ladrev@gmail.com>
2016-04-18 09:48:03 +0200

    Sortert involverte kommuner, og skilt ut per kommune involvering

--- app/directives/transactionTable.js
+++ app/directives/transactionTable.js
@@ -7,25 +7,38 @@ kommunalApp.directive('transactionTable', function($filter){
         },
         templateUrl: 'views/transactionsTableRow.html',
         link: function(scope, element, attr) {
+            scope.transaction.showInvolvementsShield = false;
+
             var involverteKommuner = scope.transaction.InvolverteKommuner.split(", ");
             var out = [];
+            var kommnr_sort_asc = function (komm1, komm2) {
+                if (komm1.kommnr > komm2.kommnr) return 1;
+                if (komm1.kommnr < komm2.kommnr) return -1;
+                return 0;
+            };
+
             angular.forEach(involverteKommuner, function(entry){
                 var kommunenr = entry.split(":")[0];
                 var kommunenavn = entry.split(":")[1];
-                out.push({kommunenr: kommunenr, kommunenavn: kommunenavn});
+                out.push({kommnr: kommunenr, kommunenavn: kommunenavn});
             })
-
+            out.sort(kommnr_sort_asc);
             scope.transaction.InvolverteKommuner = out;
+            var involveringerKommnr = [];
             var involveringer = [];
             var omsetninger = {};
             var keys = [];
 
-            function Omsetning(salgssum, dokumentdato, dokumentnr, parttype, deltagertype) {
+            function Omsetning(salgssum, dokumentdato, dokumentnr, parttype, deltagertype, kommunenr) {
                 this.salgssum = salgssum;
                 this.dokumentdato = dokumentdato;
                 this.dokumentnr = dokumentnr;
                 this.parttype = parttype;
                 this.deltagertype = deltagertype;
+                this.kommunenr = kommunenr;
+                this.year = function(){
+                    return dokumentdato.getFullYear();
+                }
                 this.isKommune = function(){return this.deltagertype == "K";}
                 this.isKjoper = function(){return this.parttype == "K";}
                 this.toString = function(){
@@ -43,7 +56,7 @@ kommunalApp.directive('transactionTable', function($filter){
                 var split = entry.split(":");
                 var currDateSplit = split[1].split("-");
                 var date = new Date(currDateSplit[0], currDateSplit[1]-1, currDateSplit[2]);
-                var omsetning = new Omsetning(split[0], date, split[2], split[3], split[4]);
+                var omsetning = new Omsetning(split[0], date, split[2], split[3], split[4], split[5]);
 
                 var currentDate = omsetninger[omsetning.dokumentdato];
                 if (currentDate == undefined){
@@ -52,7 +65,21 @@ kommunalApp.directive('transactionTable', function($filter){
                 }
 
                 if (omsetning.isKommune()){
-                    involveringer.push(omsetning);
+                    var inv;
+                    var index = involveringerKommnr.indexOf(omsetning.kommunenr);
+                    if (index != -1){
+                        inv = involveringer[index];
+                    } else {
+                        involveringerKommnr.push(omsetning.kommunenr);
+                        inv = {kommnr: omsetning.kommunenr, kommuner: [], antK:0, antS:0};                    
+                        involveringer.push(inv);
+                    }
+                    if (omsetning.isKjoper()){
+                        inv.antK = inv.antK + 1;
+                    } else {
+                        inv.antS = inv.antS + 1;
+                    };
+                    inv.kommuner.push(omsetning);
                 }
 
                 omsetninger[omsetning.dokumentdato].salgssum = omsetning.salgssum;
@@ -65,9 +92,17 @@ kommunalApp.directive('transactionTable', function($filter){
                 }
             })
 
+            
+
             var date_sort_asc = function (dato1, dato2) {
                 if (dato1 > dato2) return 1;
                 if (dato1 < dato2) return -1;
+                return 0;
+            };
+
+             var date_sort_asc_internal = function (dato1, dato2) {
+                if (dato1.dokumentdato > dato2.dokumentdato) return 1;
+                if (dato1.dokumentdato < dato2.dokumentdato) return -1;
                 return 0;
             };
 
@@ -115,6 +150,10 @@ kommunalApp.directive('transactionTable', function($filter){
             }
 
             keys.sort(date_sort_asc);
+            //involveringer.sort(date_sort_asc_internal);
+            //console.log(Object.keys(involveringer))
+            console.log(involveringer)
+
             var lastSale;
             var lastBuy;
             var nextSale;
@@ -139,7 +178,6 @@ kommunalApp.directive('transactionTable', function($filter){
             var changePerDay ="";
 
             if ((lastSale != undefined)&&(nextSale != undefined)){
-
                 if ((lastSale.salgssum != 0)&&(nextSale.salgssum !=0)){
                     var lastSalePrice= parseInt(lastSale.salgssum);
                     var nextSalePrice= parseInt(nextSale.salgssum);
@@ -153,7 +191,6 @@ kommunalApp.directive('transactionTable', function($filter){
                     var diffDays = daydiff(lastSale.dokumentdato, nextSale.dokumentdato);
                     var changePercent = changePerYear(lastSalePrice,nextSalePrice,diffDays)
     
-
                     var flagClass ="";
                     if (etterSalg > 0){
                         if (changePercent > 60){
@@ -169,9 +206,26 @@ kommunalApp.directive('transactionTable', function($filter){
                     scope.transaction.flagClass = flagClass;
                 }
             } 
+            involveringer.sort(kommnr_sort_asc);
+            angular.forEach(involveringer, function(entry){
+                entry.kommuner = entry.kommuner.sort(date_sort_asc_internal);
+                if (entry.kommuner.length > 3){
+                    var min = entry.kommuner[0].year();
+                    var max = entry.kommuner[entry.kommuner.length-1].year();
+                    if (entry.antS == entry.antK){
+                        entry.kommuner = "Kjøpt og solgt " + entry.antK +  " ganger mellom " + min + " og " + max;
+                    } else {
+                        entry.kommuner = "Kjøpt " + entry.antK + ", og solgt " + entry.antS + " ganger mellom " + min + " og " + max;
+                    }
+                    
+                } else {
+                    entry.kommuner = entry.kommuner.join(", ");
+                }
 
+                
+            });
             scope.transaction.etterSalg = etterSalg;
-            scope.transaction.involvements = involveringer.join(", ");
+            scope.transaction.involvements = involveringer;
         }
     }
 });
