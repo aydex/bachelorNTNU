a2ac07106d0a03d7db9e5e715329fd2be2a29798
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-04-21 15:17:45 +0200

    Almost similar to previous desinged tooltips

--- app/controllers/mapController.js
+++ app/controllers/mapController.js
@@ -2,11 +2,12 @@ kommunalApp.controller('mapController', function ($scope) {
     $scope.countySelected = false;
     
     $scope.move = function(evt){
-    	console.log(evt);
     	self = document.getElementsByClassName("tooltip")[0];
-    	if(self) {
-	    	self.style.left = evt.pageX - (self.offsetWidth / 2) + "px";
-	    	self.style.top = evt.pageY - 285 + "px";
+    	var rect = evt.path[0].getBoundingClientRect();
+
+    	if(self && evt.path[0].tagName == "path") {
+	    	self.style.left = rect.left + rect.width + 5 + "px";
+	    	self.style.top = rect.top - 285 + window.scrollY + rect.height + "px";
 	    	self.style.display = "block";
     	}
     }
